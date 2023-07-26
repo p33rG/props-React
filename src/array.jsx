900341034f154200289e608f295258cb9437aa69
p33rG <bkrmalik3@gmail.com>
2023-07-26 11:09:25 +0500

    props using map
    card is being displayed as an array, which uses map function to display  cards

---
+++ src/array.jsx
@@ -0,0 +1,32 @@
+const data = [
+    {
+        imgsrc: "https://static1.colliderimages.com/wordpress/wp-content/uploads/2022/11/Sonic-in-Sonic-Prime.jpg?q:50&fit:crop&w:1500&dpr:1.5",
+        name: "Sonic Prime",
+        category: "movies",
+        title: "NetFlix Orignal Series",
+        link: "google.com"
+    },
+    {
+        imgsrc: "https://static1.colliderimages.com/wordpress/wp-content/uploads/2023/07/my-happy-marriage-netflix.jpeg?q:50&fit:crop&w:1500&dpr:1.5",
+        name: "My Happy Merriage ",
+        category: "Anime",
+        title: "NetFlix Orignal Series",
+        link: "google.com",
+    },
+    {
+        imgsrc: "https://static1.colliderimages.com/wordpress/wp-content/uploads/2023/07/the-witcher-netflix-season-3-social-featured.jpg?q:50&fit:crop&w:1500&dpr:1.5",
+        name: "The Witcher",
+        category: "Movies",
+        title: "NetFlix Orignal Series",
+        link: "google.com",
+    },
+    {
+        imgsrc: "https://static1.colliderimages.com/wordpress/wp-content/uploads/2023/07/survival-of-the-thickest-netflix.jpg?q:50&fit:crop&w:1500&dpr:1.5",
+        name: "Survival of the Thickest",
+        category: "Movies",
+        title: "NetFlix Orignal Series",
+        link: "google.com",
+    }
+];
+
+export default data;
--- src/index.js
+++ src/index.js
@@ -2,41 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Cards from "./card";
 import "./style.css";
+import data from "./array";
 ReactDOM.render(
 
   <>
-    <Cards
+    <h1 className="list_header">List of top NetFlix Series</h1>
 
-      imgsrc="https://static1.colliderimages.com/wordpress/wp-content/uploads/2022/11/Sonic-in-Sonic-Prime.jpg?q=50&fit=crop&w=1500&dpr=1.5"
-      name="Sonic Prime"
-      category="movies"
-      title="NetFlix Orignal Series"
-      link="google.com"
-    />
-    <Cards
+    {data.map((card, index) => (
+      <Cards
+        key={index} // Make sure to provide a unique key for each card
+        imgsrc={card.imgsrc}
+        name={card.name}
+        category={card.category}
+        title={card.title}
+        link={card.link}
+      />
 
-      imgsrc="https://static1.colliderimages.com/wordpress/wp-content/uploads/2023/07/my-happy-marriage-netflix.jpeg?q=50&fit=crop&w=1500&dpr=1.5"
-      name="My Happy Merriage "
-      category="Anime"
-      title="NetFlix Orignal Series"
-      link="google.com"
-    />
-    <Cards
 
-      imgsrc="https://static1.colliderimages.com/wordpress/wp-content/uploads/2023/07/the-witcher-netflix-season-3-social-featured.jpg?q=50&fit=crop&w=1500&dpr=1.5"
-      name="The Witcher"
-      category="Movies"
-      title="NetFlix Orignal Series"
-      link="google.com"
-    />
-    <Cards
-
-      imgsrc="https://static1.colliderimages.com/wordpress/wp-content/uploads/2023/07/survival-of-the-thickest-netflix.jpg?q=50&fit=crop&w=1500&dpr=1.5"
-      name="Survival of the Thickest"
-      category="Movies"
-      title="NetFlix Orignal Series"
-      link="google.com"
-    />
+    ))}
 
   </>,
 
